f6eecd1ff303c41d2d7595ef0b5c3d1718dfcdca
dren <dren.bislimiapo@gmail.com>
2025-01-27 17:28:24 +0100

    cpp08/ex02 ...

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbislimi <dbislimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:22:41 by dbislimi          #+#    #+#             */
+/*   Updated: 2025/01/27 17:28:06 by dbislimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_H
+# define MUTANTSTACK_H
+
+#include <iostream>
+#include <stack>
+
+template< typename T >
+class MutantStack {
+	private:
+		std::stack<T>	_stck;
+
+	public:
+};
+
+#endif
